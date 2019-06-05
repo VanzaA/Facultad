8ed20369f65d6dddb8b4e4b752f6b3361e79a123
VanzaA <agustin.vanza@gmail.com>
2019-06-04 21:58:00 -0300

    se arreglo error segmetation fault

--- Cuarto año/Sistemas Paralelos/Entrega 2/tp-sec.c
+++ Cuarto año/Sistemas Paralelos/Entrega 2/tp-sec.c
@@ -17,7 +17,6 @@ int main(int argc, char* argv[]){
     printf("You must specify:\n\n\t- Vector size\n\t- Final value\n\t- Number of combinations");
     return 1;
   }
-
   // First argument = Vector size
   // Second argument = Final value
   // Third argument = Number of combinations
@@ -26,77 +25,65 @@ int main(int argc, char* argv[]){
   int vector_size = atoll(argv[1]);
   int value = atoi(argv[2]);
   int combinations = atoi(argv[3]); 
+  if(combinations > vector_size){
+    printf("la combinacion de numeros -n- no puede ser mayor al tamaño del vector");
+    return 1;
+  }
+
+  unsigned long long int iteraciones = 0;
   
   int sum;
-  int j = combinations;
+  int j = combinations - 1;
   int total_combinations = 0;
+  int loop = 1; 
   // Alloc memory
   int *vector = (int*) malloc(sizeof(int) * vector_size);
   int *vector_index = (int*) malloc(sizeof(int) * combinations);
   
   
   // Initialize vector
-  //printf("el vector contiene los siguientes valores[");
   for (int i = 0; i < vector_size; i++) {
     vector[i] = rand()%10;
-  //  printf("%d, ", vector[i]);
   }
-  //printf("] \n\n");
-  for(int i = 1; i <= combinations; i++){
+   
+  for(int i = 0; i < combinations; i++){
     vector_index[i] = i;
   }
+  
   
   //Start processor time
   int initial_time = dwalltime();
   
-  while(j > 0){
-    while(vector_index[j] <= (vector_size - combinations + j)){
-      sum = 0;
-      printf("[");
-      for (int i = 1; i < (combinations + 1); i++){
-        sum += vector[vector_index[i]];
-        printf("%d, ", vector[vector_index[i]]);
-      }
-      printf("]\n");
-      if(sum == value){
-        total_combinations++;
-      }
-      if(vector_index[j] == vector_size - combinations + j){
-          break;
-      }
-      vector_index[j]++;
+  while(loop){
+    iteraciones++;
+    sum = 0;
+    for (int i = 0; i < combinations; i++){
+      sum += vector[vector_index[i]];
     }
-  
-  
-  //cuando llega al final del arreglo, reseteo el valor del ultimo indice al que comenzo esta iteracion
-
-    while(vector_index[j] == (vector_size - combinations + j)){
-      vector_index[j] = vector_index[j-1]+1;
-      if(j < combinations){
-          vector_index[j+1] = vector_index[j]+1;
-      }
-      j--;
+    if(sum == value){
+      total_combinations++;
     }
-
-  //si j llega a 0, significa que se incrementaron todos al maximo
-    if(j == 0){
-      break;
-    }
-  //incremento el indice actual
     vector_index[j]++;
-
-    for(; j < combinations ;j++){
-      //si el indice actual es igual al siguiente, incremento el siguiente
-      if(vector_index[j] == vector_index[j+1]){
-          vector_index[j+1]++;
+    while(vector_index[j] == (vector_size - combinations + j + 1)){
+      if(j > 0){
+        vector_index[j - 1]++;
+        j--;
+      }
+      else{
+        loop = 0;
+        break;
       }
     }
-
+     if(loop){
+      for (; j < (combinations - 1); j++){
+        vector_index[j + 1] = vector_index[j] + 1;
+      }
+    }
   }
-
 
   printf("Time %g\n", dwalltime() - initial_time);
   printf("Total combinations = %d\n", total_combinations);
+  printf("iteraciones totales %llu\n\n", iteraciones);
 
   free(vector_index);
   free(vector);
