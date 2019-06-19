5d8b7ceb4833dc4a3c2227a5e6f395b38d891cba
VanzaA <agustin.vanza@gmail.com>
2019-06-19 00:37:13 -0300

    consultar si esta bien la implementacion

--- Cuarto año/Sistemas Paralelos/Entrega 2/mpi.c
+++ Cuarto año/Sistemas Paralelos/Entrega 2/mpi.c
@@ -1,68 +1,61 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
 #include <mpi.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/time.h>
 
-void master(int vector_size, int n, int v, int rank, int cant_proc);
-void slave(int vector_size, int n, int v, int rank, int cant_proc);
+double dwalltime(){
+  double sec;
+  struct timeval tv;
+  gettimeofday(&tv,NULL);
+  sec = tv.tv_sec + tv.tv_usec/1000000.0;
+  return sec;
+}
+
+void master(int vector_size,int *vector, int n, int v, int cant_proc);
+void slave(int vector_size,int *vector, int n, int v, int rank, int cant_proc);
+int calcular(int *vector, int n,  int v, int vector_size, int index);
 
 int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
     int rank, amount;
     MPI_Comm_size(MPI_COMM_WORLD, &amount);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (argc < 4) {
+        printf("You must specify:\n\n\t- Vector size\n\t- Final value\n\t- Number of combinations\n\n");
+        return 1;
+    }
+    
     int vector_size = atoi(argv[1]);
     int v = atoi(argv[2]);
     int n = atoi(argv[3]);    
-
-    if (argc < 4) {
-        printf("You must specify:\n\n\t- Vector size\n\t- Final value\n\t- Number of combinations");
-        return 1;
-    }
+    int *vector = (int*) malloc(sizeof(int) * vector_size);
     if(n > vector_size){
         printf("la combinacion de numeros -n- no puede ser mayor al tamaño del vector");
         return 1;
     }
 
     if (rank == 0){
-        master(vector_size, n, v, rank, amount);
+        master(vector_size, vector,n, v, amount);
     }
     else{
-        slave(vector_size, n, v, rank, amount);
+        slave(vector_size, vector, n, v, rank, amount);
     }
     
     MPI_Finalize();
     return 0;
 }
 
-void master(int vector_size, int n, int v, int rank, int cant_proc){
-    int *vector = (int*) malloc(sizeof(int) * vector_size);
-    int *vector_index = (int*) malloc(sizeof(int) * n);
-    // Initialize vector
-    for (int i = 0; i < vector_size; i++) {
-        vector[i] = rand()%10;
-    }
-    printf("master: [");
-    for (int i = 0; i < vector_size; i++){
-        printf("%d, ", vector[i]);
-    }
-    printf("]\n");  
-    
-    for(int i = 1; i < cant_proc; i++){
-        int inicio = vector_size / cant_proc * i;
-        MPI_Send(&inicio, 1, MPI_INT, i, 0, MPI_COMM_WORLD);   
-    }
-
-    MPI_Barrier(MPI_COMM_WORLD);
-    MPI_Bcast(vector, vector_size, MPI_INT, rank, MPI_COMM_WORLD);
-    for(int i = 0; i < n; i++){
-        vector_index[i] = i;
-    }
+int calcular(int *vector, int n, int v, int vector_size, int index){
     int j = n - 1;
-    int total_combinations = 0;
     int loop = 1;
-    int cant = 0;
-    unsigned long long int iteraciones = 0;
+    unsigned long int iteraciones = 0;
+    int *vector_index = (int*) malloc(sizeof(int) * n);
+    for (int i = 0; i < n; i++){
+        vector_index[i]= index + i;
+    }
+    int total_combinations = 0;
+    
     while(loop){
         iteraciones++;
         int sum = 0;
@@ -74,7 +67,7 @@ void master(int vector_size, int n, int v, int rank, int cant_proc){
         }
         vector_index[j]++;
         while(vector_index[j] == (vector_size - n + j + 1)){
-            if(j > 0){
+            if(j > 1){
                 vector_index[j - 1]++;
                 j--;
             }
@@ -83,10 +76,7 @@ void master(int vector_size, int n, int v, int rank, int cant_proc){
                 break;
             }
         }
-        if(j ==0){
-            cant++;
-        }
-        if (cant == vector_size / cant_proc){
+        if(j == 0){
             break;
         } 
         if(loop){
@@ -95,74 +85,68 @@ void master(int vector_size, int n, int v, int rank, int cant_proc){
             }
         }
     }
-
-    printf("Total combinations = %d\n", total_combinations);
-    printf("iteraciones del proceso %d totales %llu\n\n", rank,iteraciones);    
     free(vector_index);
+    return total_combinations;
+}
+
+void master(int vector_size,int *vector, int n, int v,  int cant_proc){
+    
+    int index = 0;
+    int id_slave;
+    
+    // Initialize vector
+    for (int i = 0; i < vector_size; i++) {
+        vector[i] = 1;
+    }
+    
+    MPI_Bcast(vector, vector_size, MPI_INT, 0, MPI_COMM_WORLD);
+    
+    
+    double initial_time = dwalltime();
+
+    while(index <  vector_size - n){
+        MPI_Recv(&id_slave,1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Send(&index, 1, MPI_INT, id_slave, 0, MPI_COMM_WORLD);
+        index++;
+    }
+    index = -1;
+    for(int id = 1; id < cant_proc; id++){
+        MPI_Send(&index, 1, MPI_INT, id, 0, MPI_COMM_WORLD);
+    }
+    printf("llegue\n");
+    int global_combinations  ;
+    int zero = 0;  
+    
+    MPI_Reduce(&zero, &global_combinations, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    
+    printf("proceso:(0) ---- Tiempo total = %g\n\n", dwalltime() - initial_time);
+    
+    printf("Total combinations : %d\n\n", global_combinations);
+    
     free(vector);
 }
 
-void slave(int vector_size, int n, int v, int rank, int cant_proc){
-    int *vector = (int*) malloc(sizeof(int) * vector_size);
-    int *vector_index = (int*) malloc(sizeof(int) * n);
-    
-    MPI_Barrier(MPI_COMM_WORLD);
-    MPI_Bcast(vector, vector_size, MPI_INT, 0, MPI_COMM_WORLD);
-    
-    printf("slave: [");
-    for (int i = 0; i < vector_size; i++){
-        printf("%d, ", vector[i]);
-    }
-    printf("]\n");  
-    int inicio;
-    MPI_Recv(&inicio, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    
-    for(int i = 0; i < n; i++){
-        vector_index[i] = inicio;
-        inicio ++;
-    }
-    int j = n - 1;
+void slave(int vector_size, int *vector, int n, int v, int rank, int cant_proc){
+    int index =0;
     int total_combinations = 0;
-    int loop = 1;
-    int cant = 0;
-    unsigned long long int iteraciones = 0;
-
-    while(loop){
-        iteraciones++;
-        int sum = 0;
-        for (int i = 0; i < n; i++){
-            sum += vector[vector_index[i]];
-        }
-        if(sum == v){
-            total_combinations++;
-        }
-        vector_index[j]++;
-        while(vector_index[j] == (vector_size - n + j + 1)){
-            if(j > 0){
-                vector_index[j - 1]++;
-                j--;
-            }
-            else{
-                loop = 0;
-                break;
-            }
-        }
-        if(j ==0){
-            cant++;
-        }
-        if (cant == vector_size / cant_proc){
-            break;
-        } 
-        if(loop){
-            for (; j < (n - 1); j++){
-                vector_index[j + 1] = vector_index[j] + 1;
-            }
+  
+    MPI_Bcast(vector, vector_size, MPI_INT, 0, MPI_COMM_WORLD);
+        
+    double initial_time = dwalltime();
+    while(index != -1){
+        MPI_Send(&rank, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        MPI_Recv(&index, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        if (index != -1){
+            total_combinations += calcular(vector, n, v, vector_size, index);
         }
     }
     
-    printf("Total combinations = %d\n", total_combinations);
-    printf("iteraciones del proceso %d totales %llu\n\n", rank,iteraciones);
+    
+    int global_combinations;
+    
+    MPI_Reduce(&total_combinations, &global_combinations, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    
+    printf("proceso: (%d) ---- Tiempo total = %g, combinaciones = %d\n\n", rank, dwalltime() - initial_time, total_combinations);
 
-    free(vector_index);
     free(vector);
 }
