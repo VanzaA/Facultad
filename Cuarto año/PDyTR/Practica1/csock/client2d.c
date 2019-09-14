8186dc44234c994c2f22359799b5414ae4a7b6f1
VanzaA <agustin.vanza@gmail.com>
2019-09-14 00:37:49 -0300

    2d

---
+++ Cuarto año/PDyTR/Practica1/csock/client2d.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <string.h>
+ #include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+
+void error(char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+double dwalltime()
+{
+  double sec;
+  struct timeval tv;
+
+  gettimeofday(&tv, NULL);
+  sec = tv.tv_sec + tv.tv_usec/1000000.0;
+  return sec;
+}
+
+int main(int argc, char *argv[])
+{
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    char buffer[5];
+    if (argc < 3) {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
+    }
+	//TOMA EL NUMERO DE PUERTO DE LOS ARGUMENTOS
+    portno = atoi(argv[2]);
+	
+	//CREA EL FILE DESCRIPTOR DEL SOCKET PARA LA CONEXION
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	//AF_INET - FAMILIA DEL PROTOCOLO - IPV4 PROTOCOLS INTERNET
+	//SOCK_STREAM - TIPO DE SOCKET 
+	
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+	
+	//TOMA LA DIRECCION DEL SERVER DE LOS ARGUMENTOS
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+	
+	//COPIA LA DIRECCION IP Y EL PUERTO DEL SERVIDOR A LA ESTRUCTURA DEL SOCKET
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+     serv_addr.sin_port = htons(portno);
+	
+	//DESCRIPTOR - DIRECCION - TAMAÑO DIRECCION
+    if (connect(sockfd,&serv_addr,sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
+    bzero(buffer,5);
+    
+    for(int i = 0; i< 4; i++){
+        buffer[i]='a';
+    }
+
+    double initial = dwalltime();
+
+	n = write(sockfd, buffer, strlen(buffer));
+    n = read(sockfd, buffer, strlen(buffer));
+
+    printf("time: %f \n", (dwalltime() - initial)/2);
+
+    printf("%s\n", buffer);
+    return 0;
+}
---
+++ Cuarto año/PDyTR/Practica1/csock/server2d.c
@@ -0,0 +1,81 @@
+/* A simple server in the internet domain using TCP
+   The port number is passed as an argument */
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+
+void error(char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+double dwalltime()
+{
+  double sec;
+  struct timeval tv;
+
+  gettimeofday(&tv, NULL);
+  sec = tv.tv_sec + tv.tv_usec/1000000.0;
+  return sec;
+}
+
+int main(int argc, char *argv[])
+{
+     int sockfd, newsockfd, portno, clilen;
+     char buffer[4];
+     struct sockaddr_in serv_addr, cli_addr;
+     int n;
+     if (argc < 2) {
+         fprintf(stderr,"ERROR, no port provided\n");
+         exit(1);
+     }
+	 //CREA EL FILE DESCRIPTOR DEL SOCKET PARA LA CONEXION
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	 //AF_INET - FAMILIA DEL PROTOCOLO - IPV4 PROTOCOLS INTERNET
+	//SOCK_STREAM - TIPO DE SOCKET 
+	
+     if (sockfd < 0) 
+        error("ERROR opening socket");
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+     //ASIGNA EL PUERTO PASADO POR ARGUMENTO
+	 //ASIGNA LA IP EN DONDE ESCUCHA (SU PROPIA IP)
+	 portno = atoi(argv[1]);
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_addr.s_addr = INADDR_ANY;
+     serv_addr.sin_port = htons(portno);
+	 
+	 //VINCULA EL FILE DESCRIPTOR CON LA DIRECCION Y EL PUERTO
+     if (bind(sockfd, (struct sockaddr *) &serv_addr,
+              sizeof(serv_addr)) < 0) 
+              error("ERROR on binding");
+			  
+	 //SETEA LA CANTIDAD QUE PUEDEN ESPERAR MIENTRAS SE MANEJA UNA CONEXION		  
+     listen(sockfd,5);
+	 
+	 // SE BLOQUEA A ESPERAR UNA CONEXION
+     clilen = sizeof(cli_addr);
+     newsockfd = accept(sockfd, 
+                 (struct sockaddr *) &cli_addr, 
+                 &clilen);
+				 
+     //DEVUELVE UN NUEVO DESCRIPTOR POR EL CUAL SE VAN A REALIZAR LAS COMUNICACIONES
+	 if (newsockfd < 0) 
+          error("ERROR on accept");
+     bzero(buffer,4);
+
+    n = read(sockfd, buffer, strlen(buffer));
+    n = write(sockfd, buffer, strlen(buffer));
+    
+    
+     return 0; 
+}
+
