fbfe29b850ae1530eeaa242e3e084cd0b683bb00
VanzaA <agustin.vanza@gmail.com>
2019-10-29 14:46:22 -0300

    formated files

--- Cuarto año/PDyTR/Practica 3/IfaceRemoteClass.java
+++ Cuarto año/PDyTR/Practica 3/IfaceRemoteClass.java
@@ -1,14 +1,5 @@
-/*
-* IfaceRemoteClass.java
-* Interface defining only one method which can be invoked remotely
-*
-*/
-/* Needed for defining remote method/s */
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-/* This interface will need an implementing class */
-public interface IfaceRemoteClass extends Remote
-{
-/* It will be possible to invoke this method from an application in other JVM */
-public byte[] sendThisBack(byte[] data) throws RemoteException;
+public interface IfaceRemoteClass extends Remote{
+    public byte[] sendThisBack(byte[] data) throws RemoteException;
 }
--- Cuarto año/PDyTR/Practica 3/StartRemoteObject.java
+++ Cuarto año/PDyTR/Practica 3/StartRemoteObject.java
@@ -1,25 +1,15 @@
-/*
-* StartRemoteObject.java
-* Starts the remote object. More specifically:
-* 1) Creates the object which has the remote methods to be invoked
-* 2) Registers the object so that it becomes avaliable
-*/
-import java.rmi.registry.Registry; /* REGISTRY_PORT */
-import java.rmi.Naming; /* rebind */
-public class StartRemoteObject
-{
-public static void main (String args[])
-{
-try{
-/* Create ("start") the object which has the remote method */
-RemoteClass robject = new RemoteClass();
-/* Register the object using Naming.rebind(...) */
-String rname = "//localhost:" + Registry.REGISTRY_PORT + "/remote";
-Naming.rebind(rname, robject);
-} catch (Exception e) {
-System.out.println("Hey, an error occurred at Naming.rebind");
-e.printStackTrace();
-System.out.println(e.getMessage());
-}
-}
+import java.rmi.registry.Registry;
+import java.rmi.Naming;
+public class StartRemoteObject{
+    public static void main (String args[]){
+        try{
+            RemoteClass robject = new RemoteClass();
+            String rname = "//localhost:" + Registry.REGISTRY_PORT + "/remote";
+            Naming.rebind(rname, robject);
+        } catch (Exception e) {
+            System.out.println("Hey, an error occurred at Naming.rebind");
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+    }
 }
